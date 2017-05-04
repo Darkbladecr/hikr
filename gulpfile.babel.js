c9843820d7d5d34cac412525db8ede083b841d3f
Darkbladecr <smig88@gmail.com>
2017-05-04 20:37:38 +1000

    clean dist folder before build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,21 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const del = require('del');
 const watch = require('gulp-watch');
 
 const js = 'src/**/*.js';
 const ux = 'src/**/*!(.js)';
 
-gulp.task('default', ['babel', 'assets']);
-
-gulp.task('babel', () => {
-	return gulp.src(js)
+gulp.task('default', ['clean'], () => {
+	gulp.src(js)
 		.pipe(babel())
+		.pipe(gulp.dest('dist'));
+	gulp.src(ux)
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('assets', () => {
-	return gulp.src(ux)
-		.pipe(gulp.dest('dist'));
+gulp.task('clean', () => {
+	return del('dist/');
 });
 
 gulp.task('watch', ['babel:watch', 'assets:watch']);
